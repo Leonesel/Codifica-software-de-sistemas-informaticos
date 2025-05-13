9bb8206fb437c1f2db69fe4e10037ba38a9890b8
Leonesel <navarro.eduardo.2024@cbtis-246.edu.mx>
2025-05-13 16:12:41 -0600

    Update MovieManager.py

--- MoMa proyect/MovieManager.py
+++ MoMa proyect/MovieManager.py
@@ -1,7 +1,10 @@
-#Ya no quiero estudiar
-from moviepy.editor import VideoFileClip
+#Hola
+import speech_recognition as sr
+
+from moviepy.editor import VideoFileClip, AudioFileClip #, ComposeAudioClip
 
 class MovieManager:
+        #Obtiene el audio del video
     def get_audio(self, mp4_file, mp3_file):
         vc = VideoFileClip(mp4_file)
         ac = vc.audio
@@ -17,8 +20,33 @@ class MovieManager:
         video_wa.close()
         video.close
 
-#Mozila Firefox DownlandHelper
+    def get_wav_audio(self,mp4_file,wav_file):
+        vc=VideoFileClip(mp4_file)
+        ac=vc.audio
+        ac.write_audiofile(wav_file,codec="pcm_s16le")
+        ac.close()
+        vc.close()
+
+    def audio_to_text(self,audio_file):
+        r=sr.Recognizer()
+        with sr.AudioFile(audio_file) as source:
+            audio=r.record(source)
+        try:
+            text=r.recognize_google(audio)
+            return text
+        except:
+            return "unknow"
+
+#Mozila Firefox DownlandHelper para instalar videos
+#pip install SpeechRecognition
+#pip install moviepy==1.0.3
 
 mm=MovieManager()
-mm.get_audio("video.mp4","audio.mp3") #sacar el audio
-mm.remove_audio("video.mp4","videosinaudio.mp4") #Sacarle el audio al video
+
+#mm.get_audio("video.mp4","audio.mp3") #saca el audio .mp3
+#mm.remove_audio("video.mp4","videosinaudio.mp4") #Saca el audio al video .mp4
+#mm.get_wav_audio("video.mp4","audio.wav") #Saca otro audio en .wav
+print("Transcripción del video: ")
+speech = mm.audio_to_text("audio.wav") #Transcribe el audio .wav
+print(speech)
+print("FIn de la transcripción.")
